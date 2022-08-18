715b7efa227463ff9ed1f6a59e5d211a964b9cd9
Sven Kirmess <sven.kirmess@kzone.ch>
2022-08-18 12:52:33 +0200

    - Switch license to the ISC license - Update shared files - Use redefine() instead of mock() from Test::MockModule

--- lib/Test/Pod/Links.pm
+++ lib/Test/Pod/Links.pm
@@ -1,13 +1,29 @@
-package Test::Pod::Links;
+# vim: ts=4 sts=4 sw=4 et: syntax=perl
+#
+# Copyright (c) 2018-2022 Sven Kirmess
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 use 5.006;
 use strict;
 use warnings;
 
+package Test::Pod::Links;
+
 our $VERSION = '0.003';
 
-use Carp ();
-use HTTP::Tiny 0.014 ();
+use Carp                    ();
+use HTTP::Tiny 0.014        ();
 use Pod::Simple::Search     ();
 use Pod::Simple::SimpleTree ();
 use Scalar::Util            ();
@@ -499,14 +515,4 @@ L<https://github.com/skirmess/Test-Pod-Links>
 
 Sven Kirmess <sven.kirmess@kzone.ch>
 
-=head1 COPYRIGHT AND LICENSE
-
-This software is Copyright (c) 2018-2021 by Sven Kirmess.
-
-This is free software, licensed under:
-
-  The (two-clause) FreeBSD License
-
 =cut
-
-# vim: ts=4 sts=4 sw=4 et: syntax=perl
